b2b75ede6635c518a2bc7d8d1d4947a84ca7469f
laura-richer <lauraricher@bolser.co.uk>
2018-11-22 15:06:48 +0000

    adding content and arranging shown components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import App from '../components/Global/App';
 import Button from '../components/Ui/Button';
 import { Container } from '../components/Ui/Container';
 import Questions from '../components/Questions';
 
+import content from '../src/json/content.json';
 import defaultAnswers from '../src/json/answers.json';
 
 class Home extends Component {
@@ -63,8 +64,26 @@ class Home extends Component {
           containerName="modal"
           containerWrapper="modal-header"
         >
-          {pageType === 'question' ? <Questions pageNumber={pageNumber} handleChange={this.handleRadio} handleClick={this.handleNext} /> : '' }
-          {pageType === 'final' ? <Button handleClick={this.handleResultsClick} role="button" title="Get your soundtrack" /> : '' }
+          {pageType === 'question'
+            ? (
+              <Fragment>
+                <p>{content.introCopy}</p>
+                <Questions
+                  handleChange={this.handleRadio}
+                  handleClick={this.handleNext}
+                  pageNumber={pageNumber}
+                />
+              </Fragment>
+            ) : '' }
+
+          {pageType === 'final'
+            ? (
+              <Fragment>
+                <p>{content.introCopy}</p>
+                <Button handleClick={this.handleResultsClick} role="button" title={content.submitButton} />
+              </Fragment>
+            ) : '' }
+
           {pageType === 'results' ? <p>results here</p> : '' }
         </Container>
       </App>
